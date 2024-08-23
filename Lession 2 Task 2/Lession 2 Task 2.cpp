5ef81b0c365738e2bd3acf73a3603bb9d425d14f
VIrus7-bit <italyfotura7@gmail.com>
2024-08-23 09:24:12 -0700

    Corrected lesson 2 task 2

---
+++ Lession 2 Task 2/Lession 2 Task 2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+class Counter {
+private:
+    int num;
+public:
+    Counter() :num(1) {}
+    Counter(int start_value) :num(start_value) {}
+    void increase() { num++; }
+    void decrease() { num--; }
+    int count() const {std::cout << num << std::endl; return num; }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+    int  a = 0;
+    char s;
+
+    std::string ptr1 = "да";
+    std::string ptr2;
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> ptr2;
+
+    if (ptr1 == ptr2) {
+
+        std::cout << "Введите начальное значения счётчика: ";
+        std::cin >> a;
+    }
+    else {
+        a = 1;
+    }
+    Counter set{a};
+
+    do {std::cout << "Введите команду ('+' , '-' , '=' или 'x'): ";
+        std::cin >> s;
+
+        switch (s){
+        case '+': { set.increase(); } break;
+        case '-': { set.decrease(); } break;
+        case '=': { set.count   (); } break;}
+    } while (s != 'x'); std::cout << "Досвидания!";
+}
